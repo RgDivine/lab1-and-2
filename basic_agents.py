078f33db407a907bb143f236beff1ee0ccdd4f69
RgDivine <151162575+RgDivine@users.noreply.github.com>
2026-01-29 10:53:58 +0000

    1

--- basic_agents.py
+++ basic_agents.py
@@ -1,1 +1,19 @@
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+import asyncio
 
+class BasicAgent(Agent):
+    class HelloBehaviour(CyclicBehaviour):
+        async def run(self):
+            print(f"{self.agent.jid} is running...")
+            await asyncio.sleep(5)
+
+    async def setup(self):
+        print("BasicAgent starting...")
+        self.add_behaviour(self.HelloBehaviour())
+
+if __name__ == "__main__":
+    agent = BasicAgent("basicagent@localhost", "password")
+    agent.start()
+    input("Press ENTER to stop the agent")
+    agent.stop()
